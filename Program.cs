de7d3906e8f5b49715134ba65ed565e0cccd95ba
Andrea Tauberová <andrea.tauberova@seznam.cz>
2024-01-26 21:02:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Obesenec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Slovo slovo = new Slovo();
+            slovo.VymysliNoveSlovo();
+            int pocetPismen = slovo.PocetPismen();
+            Console.WriteLine($"Vítám tě u hry Oběšenec. Myslím si slovo. Hádej počet písmen: {pocetPismen}");
+            // cyklus dokud nebudou uhadnuta vsechna pismena
+        }
+    }
+}
---
+++ Slovo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obesenec
+{
+    internal class Slovo
+    {
+        private string[] seznamSlov = new string[] {"prase", "kočka", "vlak", "program", "víno", "počítač"};
+        private string hadaneSlovo;
+        private char[] castecneOdhaleneSlovo;
+
+        public Slovo()
+        {
+            VymysliNoveSlovo();
+        }
+
+        public void VymysliNoveSlovo ()
+        {
+            Random nahodnyVyber = new Random();
+            hadaneSlovo = seznamSlov[nahodnyVyber.Next(0, seznamSlov.Length)];
+        }
+
+        public int PocetPismen()
+        { 
+            return hadaneSlovo.Length; 
+        }
+
+        public void VratSlovo()
+        {
+            //metoda na ReadKey + cyklus a zobrazovani uhadnutych pismen do slova
+        }
+        
+    }
+}
